8d96af01b59b4be4ca72d8a62205e98bef8bbd9c
Jake <jake@evansturner.co.uk>
2014-06-09 04:55:48 +0100

    comment about a good reference for algorithms

--- lpa.h
+++ lpa.h
@@ -4,6 +4,8 @@ Large precision arithmetic functions
 #ifndef LPA_H
 #define LPA_H
 
+/* See "The art of computer programming" by Knuth Vol 2 p.250 */
+
 #include "lpa_core.h"
 #include "lpa_bcd.h"
 #include "lpa_int.h"
