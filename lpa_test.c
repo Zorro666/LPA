2227e83a51613e2bbbbcd1bc86ddf93c1f536ac7
Jake <jake@evansturner.co.uk>
2014-06-09 04:59:13 +0100

    tests for LPA_BCD_multiply() valgrind errors about leaking memory, free the temp test numbers when they get re-created

--- lpa_test.c
+++ lpa_test.c
@@ -17,11 +17,6 @@ void LPA_freeMem(void* pMem)
 
 #define CHAR_BUFFER_SIZE (2048)
 
-static void printLPATestResult(const char* const name, const char* const lpaResult, long a, const char* const op, long b, long spResult)
-{
-	printf("%s:\t0x%s 0x%lX %s 0x%lX = 0x%lX\n", name, lpaResult, a, op, b, spResult);
-}
-
 typedef void(LPA_BCD_func)(const LPA_BCD_number* const pA, const LPA_BCD_number* const pB, LPA_BCD_number* const pResult);
 typedef void(LPA_INT_func)(const LPA_INT_number* const pA, const LPA_INT_number* const pB, LPA_INT_number* const pResult);
 
@@ -37,8 +32,13 @@ static int doLPA_BCDTest(const char* const test, LPA_BCD_func testFunc, const lo
 	long result = 0;
 	int success = 1;
 
+	LPA_BCD_initNumber(&lpaResult);
+	LPA_BCD_initNumber(&lpaA);
+	LPA_BCD_initNumber(&lpaB);
+
 	LPA_BCD_fromInt64(&lpaA, a);
 	LPA_BCD_fromInt64(&lpaB, b);
+	LPA_BCD_initNumber(&lpaResult);
 
 	if (testFunc == LPA_BCD_add)
 	{
@@ -47,6 +47,10 @@ static int doLPA_BCDTest(const char* const test, LPA_BCD_func testFunc, const lo
 	else if (testFunc == LPA_BCD_subtract)
 	{
 		result = a - b;
+	}
+	else if (testFunc == LPA_BCD_multiply)
+	{
+		result = a * b;
 	}
 
 	testFunc(&lpaA, &lpaB, &lpaResult);
@@ -65,6 +69,7 @@ static int doLPA_BCDTest(const char* const test, LPA_BCD_func testFunc, const lo
 	{
 		success = 0;
 	}
+	LPA_BCD_freeNumber(&lpaResult);
 	LPA_BCD_fromInt64(&lpaResult, result);
 	LPA_BCD_toDecimalASCII(&lpaResult, resultTruth, CHAR_BUFFER_SIZE);
 	if (strcmp(resultTruth, outResult) != 0)
@@ -77,13 +82,17 @@ static int doLPA_BCDTest(const char* const test, LPA_BCD_func testFunc, const lo
 				test, outResult, result, outA, test, outB, outResult, a, test, b, result);
 		fprintf(stderr, "TEST FAILED '%s' : %ld %s %ld = %s != %s\n", test, a, test, b, resultTruth, outResult);
 	}
+
+	LPA_BCD_freeNumber(&lpaResult);
+	LPA_BCD_freeNumber(&lpaA);
+	LPA_BCD_freeNumber(&lpaB);
 	return success;
 }
 
-static int doLPA_BCDRandomTests(const int numTests)
+static int doLPA_BCDRandomTests(const long numTests)
 {
 	int success = 1;
-	int i = 0;
+	long i = 0;
 
 	while ((i < numTests) && (success == 1))
 	{
@@ -114,6 +123,9 @@ static int doLPA_INTTest(const char* const test, LPA_INT_func testFunc, const lo
 	long result = 0;
 	int success = 1;
 
+	LPA_INT_initNumber(&lpaResult);
+	LPA_INT_initNumber(&lpaA);
+	LPA_INT_initNumber(&lpaB);
 	LPA_INT_fromInt64(&lpaA, a);
 	LPA_INT_fromInt64(&lpaB, b);
 
@@ -142,6 +154,7 @@ static int doLPA_INTTest(const char* const test, LPA_INT_func testFunc, const lo
 	{
 		success = 0;
 	}
+	LPA_INT_freeNumber(&lpaResult);
 	LPA_INT_fromInt64(&lpaResult, result);
 	LPA_INT_toHexadecimalASCII(&lpaResult, resultTruth, CHAR_BUFFER_SIZE);
 	if (strcmp(resultTruth, outResult) != 0)
@@ -154,13 +167,17 @@ static int doLPA_INTTest(const char* const test, LPA_INT_func testFunc, const lo
 				test, outResult, result, outA, test, outB, outResult, a, test, b, result);
 		fprintf(stderr, "TEST FAILED '%s' : 0x%lX %s 0x%lX = 0x%s != 0x%s\n", test, a, test, b, resultTruth, outResult);
 	}
+
+	LPA_INT_freeNumber(&lpaResult);
+	LPA_INT_freeNumber(&lpaA);
+	LPA_INT_freeNumber(&lpaB);
 	return success;
 }
 
-static int doLPA_INTRandomTests(const int numTests)
+static int doLPA_INTRandomTests(const long numTests)
 {
 	int success = 1;
-	int i = 0;
+	long i = 0;
 
 	while ((i < numTests) && (success == 1))
 	{
@@ -190,27 +207,32 @@ static int testBCD(const int argc, char** argv)
 {
 	LPA_BCD_number testNumber;
 	LPA_BCD_number aNumber;
-	LPA_BCD_number bNumber;
 	LPA_BCD_number resultNumber;
 	LPA_BCD_number tempNumber;
 	int inA32 = 166;
 	long inA = 166;
 	long inB = 166;
-	long temp = 0;
-	long result = 0;
+	long numTests = 1;
 	char outBuffer[CHAR_BUFFER_SIZE];
 
-	printf("################# BCD Tests #################\n");
+	LPA_BCD_initNumber(&testNumber);
+	LPA_BCD_initNumber(&aNumber);
 	LPA_BCD_initNumber(&resultNumber);
 	LPA_BCD_initNumber(&tempNumber);
 	if (argc > 1)
 	{
-		inA = atol(argv[1]);
+		numTests = atol(argv[1]);
 	}
 	if (argc > 2)
 	{
-		inB = atol(argv[2]);
+		inA = atol(argv[2]);
 	}
+	if (argc > 3)
+	{
+		inB = atol(argv[3]);
+	}
+	printf("################# BCD Tests : %lu #################\n", numTests);
+
 	printf("inNumber:%ld\n", inA);
 	LPA_BCD_fromInt64(&aNumber, inA);
 	printf("numDigits:%u\n", aNumber.numDigits);
@@ -225,129 +247,74 @@ static int testBCD(const int argc, char** argv)
 
 	if (argc > 1)
 	{
+		LPA_BCD_freeNumber(&testNumber);
 		LPA_BCD_fromDecimalASCII(&testNumber, argv[1]);
 		LPA_BCD_toDecimalASCII(&testNumber, outBuffer, CHAR_BUFFER_SIZE);
 		printf("'%s' outASCII:%s\n", argv[1], outBuffer);
 	}
 	if (argc > 2)
 	{
+		LPA_BCD_freeNumber(&testNumber);
 		LPA_BCD_fromDecimalASCII(&testNumber, argv[2]);
 		LPA_BCD_toDecimalASCII(&testNumber, outBuffer, CHAR_BUFFER_SIZE);
 		printf("'%s' outASCII:%s\n", argv[2], outBuffer);
 	}
 
 	inA32 = (int)(inA);
+	LPA_BCD_freeNumber(&testNumber);
 	LPA_BCD_fromInt32(&testNumber, inA32);
 	LPA_BCD_toDecimalASCII(&testNumber, outBuffer, CHAR_BUFFER_SIZE);
 	printf("inA32:%d\n", inA32);
 	printf("out:%s\n", outBuffer);
 
-	LPA_BCD_fromInt64(&aNumber, inA);
-	LPA_BCD_toDecimalASCII(&aNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("a:%s\n", outBuffer);
+	LPA_BCD_freeNumber(&testNumber);
+	LPA_BCD_freeNumber(&aNumber);
+	LPA_BCD_freeNumber(&resultNumber);
+	LPA_BCD_freeNumber(&tempNumber);
 
-	LPA_BCD_fromInt64(&bNumber, inB);
-	LPA_BCD_toDecimalASCII(&bNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("b:%s\n", outBuffer);
+	if (doLPA_BCDTest("*", LPA_BCD_multiply, inA, inB) == 0)
+	{
+		return 0;
+	}
+	if (doLPA_BCDTest("+", LPA_BCD_add, inA, inB) == 0)
+	{
+		return 0;
+	}
 
-	LPA_BCD_add(&aNumber, &bNumber, &resultNumber);
-	temp = result;
-	result = inA + inB;
-	LPA_BCD_toDecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("addAB:%s %ld + %ld = %ld\n", outBuffer, inA, inB, result);
-
-	LPA_BCD_subtract(&aNumber, &bNumber, &resultNumber);
-	temp = result;
-	result = inA - inB;
-	LPA_BCD_toDecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("subAB:%s %ld - %ld = %ld\n", outBuffer, inA, inB, result);
-	temp = result;
-
-	LPA_BCD_add(&aNumber, &resultNumber, &resultNumber);
-	temp = result;
-	result = inA + temp;
-	LPA_BCD_toDecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("add:%s %ld + %ld = %ld\n", outBuffer, inA, temp, result);
-
-	LPA_BCD_add(&resultNumber, &resultNumber, &resultNumber);
-	temp = result;
-	result = temp + temp;
-	LPA_BCD_toDecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("add:%s %ld + %ld = %ld\n", outBuffer, temp, temp, result);
-
-	LPA_BCD_add(&resultNumber, &bNumber, &resultNumber);
-	temp = result;
-	result = temp + inB;
-	LPA_BCD_toDecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("add:%s %ld + %ld = %ld\n", outBuffer, temp, inB, result);
-
-	LPA_BCD_add(&resultNumber, &bNumber, &resultNumber);
-	temp = result;
-	result = temp + inB;
-	LPA_BCD_toDecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("add:%s %ld + %ld = %ld\n", outBuffer, temp, inB, result);
-
-	LPA_BCD_subtract(&resultNumber, &bNumber, &resultNumber);
-	temp = result;
-	result = temp - inB;
-	LPA_BCD_toDecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("sub:%s %ld - %ld = %ld\n", outBuffer, temp, inB, result);
-
-	LPA_BCD_subtract(&resultNumber, &bNumber, &resultNumber);
-	temp = result;
-	result = temp - inB;
-	LPA_BCD_toDecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("sub:%s %ld - %ld = %ld\n", outBuffer, temp, inB, result);
-
-	LPA_BCD_subtract(&aNumber, &resultNumber, &resultNumber);
-	temp = result;
-	result = inA - temp;
-	LPA_BCD_toDecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("sub:%s %ld - %ld = %ld\n", outBuffer, inA, temp, result);
-
-	LPA_BCD_subtract(&bNumber, &resultNumber, &resultNumber);
-	temp = result;
-	result = inB - temp;
-	LPA_BCD_toDecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("sub:%s %ld - %ld = %ld\n", outBuffer, inB, temp, result);
-
-	LPA_BCD_subtract(&resultNumber, &resultNumber, &resultNumber);
-	temp = result;
-	result = temp - temp;
-	LPA_BCD_toDecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("sub:%s %ld - %ld = %ld\n", outBuffer, temp, temp, result);
-
-	doLPA_BCDTest("+", LPA_BCD_add, inA, inB);
-
-	return doLPA_BCDRandomTests(1000000);
+	return doLPA_BCDRandomTests(numTests);
 }
 
 static int testINT(const int argc, char** argv)
 {
 	LPA_INT_number testNumber;
 	LPA_INT_number aNumber;
-	LPA_INT_number bNumber;
 	LPA_INT_number resultNumber;
 	LPA_INT_number tempNumber;
 	int inA32 = 166;
 	long inA = 166;
 	long inB = 166;
-	long temp = 0;
 	long result = 0;
+	long numTests = 1;
 	char outBuffer[CHAR_BUFFER_SIZE];
 
-	printf("################# INT Tests #################\n");
-	
+	LPA_INT_initNumber(&testNumber);
+	LPA_INT_initNumber(&aNumber);
 	LPA_INT_initNumber(&resultNumber);
 	LPA_INT_initNumber(&tempNumber);
 	if (argc > 1)
 	{
-		inA = atol(argv[1]);
+		numTests = atol(argv[1]);
 	}
 	if (argc > 2)
 	{
-		inB = atol(argv[2]);
+		inA = atol(argv[2]);
 	}
+	if (argc > 3)
+	{
+		inB = atol(argv[3]);
+	}
+	printf("################# INT Tests : %lu #################\n", numTests);
+
 	printf("inNumber:%ld\n", inA);
 	LPA_INT_fromInt64(&aNumber, inA);
 	printf("numDigits:%u\n", aNumber.numDigits);
@@ -356,114 +323,44 @@ static int testINT(const int argc, char** argv)
 	printf("in:%ld\n", inA);
 	printf("out:0x%s\n", outBuffer);
 
+	LPA_INT_freeNumber(&testNumber);
 	LPA_INT_fromHexadecimalASCII(&testNumber, "100");
 	LPA_INT_toHexadecimalASCII(&testNumber, outBuffer, CHAR_BUFFER_SIZE);
 	printf("'%s' outASCII:0x%s %d 0x%X\n", "100", outBuffer, testNumber.numDigits, testNumber.pDigits[0]);
 
 	if (argc > 1)
 	{
+		LPA_INT_freeNumber(&testNumber);
 		LPA_INT_fromHexadecimalASCII(&testNumber, argv[1]);
 		LPA_INT_toHexadecimalASCII(&testNumber, outBuffer, CHAR_BUFFER_SIZE);
 		printf("'%s' %ld 0x%lX outASCII:0x%s %d 0x%X\n", argv[1], inA, (unsigned long)inA, outBuffer, testNumber.numDigits, testNumber.pDigits[0]);
 	}
 	if (argc > 2)
 	{
+		LPA_INT_freeNumber(&testNumber);
 		LPA_INT_fromHexadecimalASCII(&testNumber, argv[2]);
 		LPA_INT_toHexadecimalASCII(&testNumber, outBuffer, CHAR_BUFFER_SIZE);
 		printf("'%s' %ld 0x%lX outASCII:0x%s %d 0x%X\n", argv[2], inB, (unsigned long)inB, outBuffer, testNumber.numDigits, testNumber.pDigits[0]);
 	}
 
 	inA32 = (int)(inA);
+	LPA_INT_freeNumber(&testNumber);
 	LPA_INT_fromInt32(&testNumber, inA32);
 	result = inA;
 	LPA_INT_toHexadecimalASCII(&testNumber, outBuffer, CHAR_BUFFER_SIZE);
 	printf("out a32:\t0x%s 0x%lX %d 0x%X\n", outBuffer, (unsigned long)result, testNumber.numDigits, testNumber.pDigits[0]);
 
-	LPA_INT_fromInt64(&aNumber, inA);
-	result = inA;
-	LPA_INT_toHexadecimalASCII(&aNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("out a:\t0x%s 0x%lX %d 0x%X\n", outBuffer, (unsigned long)result, aNumber.numDigits, aNumber.pDigits[0]);
+	LPA_INT_freeNumber(&testNumber);
+	LPA_INT_freeNumber(&aNumber);
+	LPA_INT_freeNumber(&resultNumber);
+	LPA_INT_freeNumber(&tempNumber);
 
-	LPA_INT_fromInt64(&bNumber, inB);
-	result = inB;
-	LPA_INT_toHexadecimalASCII(&bNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printf("out b:\t0x%s 0x%lX %d 0x%X\n", outBuffer, (unsigned long)result, bNumber.numDigits, bNumber.pDigits[0]);
+	if (doLPA_INTTest("+", LPA_INT_add, inA, inB) == 0)
+	{
+		return 0;
+	}
 
-	LPA_INT_add(&aNumber, &bNumber, &resultNumber);
-	temp = result;
-	result = inA + inB;
-	LPA_INT_toHexadecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printLPATestResult("addAB", outBuffer, inA, "+", inB, result);
-
-	LPA_INT_add(&aNumber, &resultNumber, &resultNumber);
-	temp = result;
-	result = inA + temp;
-	LPA_INT_toHexadecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printLPATestResult("addABA", outBuffer, inA, "+", temp, result);
-
-	LPA_INT_subtract(&aNumber, &bNumber, &resultNumber);
-	temp = result;
-	result = inA - inB;
-	LPA_INT_toHexadecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printLPATestResult("subAB", outBuffer, inA, "-", inB, result);
-
-	LPA_INT_add(&aNumber, &resultNumber, &resultNumber);
-	temp = result;
-	result = inA + temp;
-	LPA_INT_toHexadecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printLPATestResult("add", outBuffer, inA, "+", temp, result);
-
-	LPA_INT_add(&resultNumber, &resultNumber, &resultNumber);
-	temp = result;
-	result = temp + temp;
-	LPA_INT_toHexadecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printLPATestResult("add", outBuffer, temp, "+", temp, result);
-
-	LPA_INT_add(&resultNumber, &bNumber, &resultNumber);
-	temp = result;
-	result = temp + inB;
-	LPA_INT_toHexadecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printLPATestResult("add", outBuffer, temp, "+", inB, result);
-
-	LPA_INT_add(&resultNumber, &bNumber, &resultNumber);
-	temp = result;
-	result = temp + inB;
-	LPA_INT_toHexadecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printLPATestResult("add", outBuffer, temp, "+", inB, result);
-
-	LPA_INT_subtract(&resultNumber, &bNumber, &resultNumber);
-	temp = result;
-	result = temp - inB;
-	LPA_INT_toHexadecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printLPATestResult("sub", outBuffer, temp, "-", inB, result);
-
-	LPA_INT_subtract(&resultNumber, &bNumber, &resultNumber);
-	temp = result;
-	result = temp - inB;
-	LPA_INT_toHexadecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printLPATestResult("sub", outBuffer, temp, "-", inB, result);
-
-	LPA_INT_subtract(&aNumber, &resultNumber, &resultNumber);
-	temp = result;
-	result = inA - temp;
-	LPA_INT_toHexadecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printLPATestResult("sub", outBuffer, inA, "-", temp, result);
-
-	LPA_INT_subtract(&bNumber, &resultNumber, &resultNumber);
-	temp = result;
-	result = inB - temp;
-	LPA_INT_toHexadecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printLPATestResult("sub", outBuffer, inB, "-", temp, result);
-
-	LPA_INT_subtract(&resultNumber, &resultNumber, &resultNumber);
-	temp = result;
-	result = temp - temp;
-	LPA_INT_toHexadecimalASCII(&resultNumber, outBuffer, CHAR_BUFFER_SIZE);
-	printLPATestResult("sub", outBuffer, temp, "-", temp, result);
-
-	doLPA_INTTest("+", LPA_INT_add, inA, inB);
-
-	return doLPA_INTRandomTests(1000000);
+	return doLPA_INTRandomTests(numTests);
 }
 
 int main(const int argc, char** argv)
